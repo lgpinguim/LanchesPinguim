ed75b5b2acc71c5381d3859821e1c938f1db27c4
Luis Ferreira <lg_pinguim@hotmail.com>
2023-01-12 19:36:19 -0300

    Project creation and ajustments

--- LanchesPinguim/Controllers/HomeController.cs
+++ LanchesPinguim/Controllers/HomeController.cs
@@ -8,17 +8,7 @@ namespace LanchesPinguim.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
